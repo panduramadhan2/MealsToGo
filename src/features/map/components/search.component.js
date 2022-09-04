1991229bbb012e61c7183101da5c2383536a5c19
panduramadhan2 <panduramadhan2@gmail.com>
2022-09-04 18:29:23 +0700

    184

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -6,13 +6,9 @@ import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
-  position: absolute;
-  z-index: 999;
-  top: 40px;
-  width: 100%;
 `;
 
-export const Search = () => {
+export const Search = ({ isFavouritesToggled, onFavouritesToggle }) => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
   useEffect(() => {
@@ -21,8 +17,9 @@ export const Search = () => {
   return (
     <SearchContainer>
       <Searchbar
+        icon={isFavouritesToggled ? "heart" : "heart-outline"}
+        onIconPress={onFavouritesToggle}
         placeholder="Search for a location"
-        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
