8401e6302482b0718e00f78ecd0f7ae17f45d71d
panduramadhan2 <panduramadhan2@gmail.com>
2022-10-03 23:29:14 +0700

    217 implement camera expo

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screens";
+import { CameraScreen } from "../../features/settings/screens/camera.screen";
 
 import {
   createStackNavigator,
@@ -12,8 +13,8 @@ const SettingsStack = createStackNavigator();
 export const SettingsNavigator = ({ route, navigation }) => {
   return (
     <SettingsStack.Navigator
+      headerMode="screen"
       screenOptions={{
-        headerMode: "screen",
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
       }}
     >
@@ -25,6 +26,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+      <SettingsStack.Screen name="Camera" component={CameraScreen} />
     </SettingsStack.Navigator>
   );
 };
