68041285318d0ce97a2a5578c8547808ce7b5611
panduramadhan2 <panduramadhan2@gmail.com>
2022-10-04 01:14:37 +0700

    220 camera fiture with button papper done

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,21 +1,47 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
 import { Camera, CameraType } from "expo-camera";
 import styled from "styled-components/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { View } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+
+import { Button } from "react-native-paper";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+  flex: 1;
 `;
 
-export const CameraScreen = () => {
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  top: 525px;
+  left: 140px;
+`;
+
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      //   console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
     })();
   }, []);
@@ -29,7 +55,13 @@ export const CameraScreen = () => {
   return (
     <ProfileCamera
       ref={(camera) => (cameraRef.current = camera)}
+      ratio={"16:9"}
       type={CameraType.front}
-    ></ProfileCamera>
+      onCameraReady={() => {
+        console.log("Camera Ready");
+      }}
+    >
+      <CameraButton onPress={snap}>Snap!</CameraButton>
+    </ProfileCamera>
   );
 };
